a90c90c93ce9f8c1aff6b2a7d16f2b030ace5968
陈华榕 <chen.huarong@chinaredstar.com>
2020-08-16 11:10:00 +0800

    Initial commit

---
+++ src/test/java/com/rong/demo/BlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rong.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
